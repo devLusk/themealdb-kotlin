f67a6123d37cb451df35bc02cc84706d1b485083
Lucas <lucasvida2005@gmail.com>
2025-10-08 11:11:55 -0300

    feat: scaffold RecipeScreen

---
+++ app/src/main/java/com/devlusk/recipeapp/RecipeScreen.kt
@@ -0,0 +1,9 @@
+package com.devlusk.recipeapp
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun RecipeScreen(modifier: Modifier = Modifier) {
+    
+}
